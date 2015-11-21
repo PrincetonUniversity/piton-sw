c31635df230e3a431383a92031f35b318ea52e51
Matthew Matl <mmatl@princeton.edu>
2015-11-21 16:29:21 -0500

    Fix Multiplier

--- subos/hypervisor/src/greatlakes/ontario/include/platform/fpga.h
+++ subos/hypervisor/src/greatlakes/ontario/include/platform/fpga.h
@@ -2,43 +2,43 @@
 * ========== Copyright Header Begin ==========================================
 *
 * Hypervisor Software File: fpga.h
-* 
+*
 * Copyright (c) 2006 Sun Microsystems, Inc. All Rights Reserved.
-* 
+*
 *  - Do no alter or remove copyright notices
-* 
-*  - Redistribution and use of this software in source and binary forms, with 
-*    or without modification, are permitted provided that the following 
-*    conditions are met: 
-* 
-*  - Redistribution of source code must retain the above copyright notice, 
+*
+*  - Redistribution and use of this software in source and binary forms, with
+*    or without modification, are permitted provided that the following
+*    conditions are met:
+*
+*  - Redistribution of source code must retain the above copyright notice,
 *    this list of conditions and the following disclaimer.
-* 
+*
 *  - Redistribution in binary form must reproduce the above copyright notice,
 *    this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution. 
-* 
-*    Neither the name of Sun Microsystems, Inc. or the names of contributors 
-* may be used to endorse or promote products derived from this software 
-* without specific prior written permission. 
-* 
-*     This software is provided "AS IS," without a warranty of any kind. 
-* ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, 
-* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A 
-* PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN 
-* MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR 
-* ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR 
-* DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN 
-* OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR 
-* FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE 
-* DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, 
-* ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF 
+*    documentation and/or other materials provided with the distribution.
+*
+*    Neither the name of Sun Microsystems, Inc. or the names of contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+*     This software is provided "AS IS," without a warranty of any kind.
+* ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN
+* MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR
+* ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+* DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN
+* OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR
+* FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE
+* DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY,
+* ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF
 * SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* 
+*
 * You acknowledge that this software is not designed, licensed or
 * intended for use in the design, construction, operation or maintenance of
-* any nuclear facility. 
-* 
+* any nuclear facility.
+*
 * ========== Copyright Header End ============================================
 */
 /*
@@ -59,7 +59,7 @@ extern "C" {
 #define	FPGA_SRAM_BASE		MByte(8)
 #define	FPGA_INTR_BASE		HOST_REGS_BASE(0x0a000)
 
-#define	UART_CLOCK_MULTIPLIER	8 /* For Niagara FPGA */
+#define	UART_CLOCK_MULTIPLIER	1 /* For Niagara FPGA */
 #define	FPGA_UART_BASE		(FPGA_BASE + 0xc2c000)
 
 /* mbox/queue offsets */
