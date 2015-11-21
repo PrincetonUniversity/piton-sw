f3295bfc0c93b9aa4b1e24d6d991e48bdac3a19c
Matthew Matl <mmatl@princeton.edu>
2015-11-21 16:30:31 -0500

    Attempt to up baud to 9600

--- subos/hypervisor/src/include/devices/pc16550.h
+++ subos/hypervisor/src/include/devices/pc16550.h
@@ -2,43 +2,43 @@
 * ========== Copyright Header Begin ==========================================
 *
 * Hypervisor Software File: pc16550.h
-* 
+*
 * Copyright (c) 2006 Sun Microsystems, Inc. All Rights Reserved.
-* 
+*
 *  - Do no alter or remove copyright notices
-* 
-*  - Redistribution and use of this software in source and binary forms, with 
-*    or without modification, are permitted provided that the following 
-*    conditions are met: 
-* 
-*  - Redistribution of source code must retain the above copyright notice, 
+*
+*  - Redistribution and use of this software in source and binary forms, with
+*    or without modification, are permitted provided that the following
+*    conditions are met:
+*
+*  - Redistribution of source code must retain the above copyright notice,
 *    this list of conditions and the following disclaimer.
-* 
+*
 *  - Redistribution in binary form must reproduce the above copyright notice,
 *    this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution. 
-* 
-*    Neither the name of Sun Microsystems, Inc. or the names of contributors 
-* may be used to endorse or promote products derived from this software 
-* without specific prior written permission. 
-* 
-*     This software is provided "AS IS," without a warranty of any kind. 
-* ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES, 
-* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A 
-* PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN 
-* MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR 
-* ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR 
-* DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN 
-* OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR 
-* FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE 
-* DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, 
-* ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF 
+*    documentation and/or other materials provided with the distribution.
+*
+*    Neither the name of Sun Microsystems, Inc. or the names of contributors
+* may be used to endorse or promote products derived from this software
+* without specific prior written permission.
+*
+*     This software is provided "AS IS," without a warranty of any kind.
+* ALL EXPRESS OR IMPLIED CONDITIONS, REPRESENTATIONS AND WARRANTIES,
+* INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A
+* PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE HEREBY EXCLUDED. SUN
+* MICROSYSTEMS, INC. ("SUN") AND ITS LICENSORS SHALL NOT BE LIABLE FOR
+* ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+* DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES. IN NO EVENT WILL SUN
+* OR ITS LICENSORS BE LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR
+* FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE
+* DAMAGES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY,
+* ARISING OUT OF THE USE OF OR INABILITY TO USE THIS SOFTWARE, EVEN IF
 * SUN HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
-* 
+*
 * You acknowledge that this software is not designed, licensed or
 * intended for use in the design, construction, operation or maintenance of
-* any nuclear facility. 
-* 
+* any nuclear facility.
+*
 * ========== Copyright Header End ============================================
 */
 /*
@@ -96,6 +96,6 @@ extern "C" {
 /*
  * Baud rate settings for Divisor Latch Low (DLL) and Most (DLM)
  */
-#define	DLL_9600	0xbc
+#define	DLL_9600	0x0F
 #define	DLM_9600	0x0
 #endif /* _PC16550_H */
