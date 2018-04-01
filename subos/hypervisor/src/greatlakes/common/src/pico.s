3c43f1ac5d133d536124fabd2861c4e174975afb
Katie Lim <katiemlim@gmail.com>
2018-03-31 23:00:05 -0400

    Updating pico code

--- subos/hypervisor/src/greatlakes/common/src/pico.s
+++ subos/hypervisor/src/greatlakes/common/src/pico.s
@@ -10,27 +10,29 @@
 #define PITON_VINT_X_OFFSET 18
 #define PITON_INT_RST_MSG   0x8000000000010001
 
-#define PICO_STATUS 0x3F000000
+#define PICO_STATUS_ADDR 0x3F000090
+#define PICO_DATA   0x41424344
 
+/* arg1: %o0 physical address
+ *
+ */
 ENTRY_NP(hcall_pico_start)
-    mov %g7, %g3
     PRINT("Entered pico hypercall\r\n")
-    mov %g3, %g7
+    PRINT_REGISTER("Got physical address ", %o0)
+    PRINT("\r\n")
+    lduw [%o0], %g1
+    PRINT_REGISTER("Got value", %g1)
+    PRINT("\r\n")
+
+
     setx PITON_INT_RST_MSG,%g1, %g3
     mov  1, %g1
     sll  %g1, PITON_VINT_X_OFFSET, %g1
     or   %g3, %g1, %g3
     setx PITON_INT_ADDR, %g1, %g2
     stx  %g3, [%g2]
+    PRINT("Started pico\r\n")
 
-    setx PICO_STATUS, %g1, %g2
-poll_loop:
-    ldub [%g2], %g3
-    cmp  %g3, 0xff
-    bne  poll_loop
-    
-    PRINT_REGISTER("Got value: ", %g3)
-    PRINT("\r\n")
     HCALL_RET(EOK)
 SET_SIZE(hcall_pico_start)
  
